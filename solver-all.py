b41fea526c6e16e88201c8d63160b1ae2c47aef4
WanderingPhilosopher <65258474+WanderingPhilosopher@users.noreply.github.com>
2021-01-09 23:39:05 -0600

    Add files via upload

--- solver-all.py
+++ solver-all.py
@@ -3,9 +3,9 @@
 import sys
 
 
-def comparatora(tame0, tame1, tame2, tame3, wild0, wild1, wild2, wild3):
+def comparatora(tame0, wild0):
     A, Ak, B, Bk = [], [], [], []
-    with open('tame-0.txt', 'r') as f:
+    with open('check1.txt', 'r') as f:
         for line in f:
             if len(line) == 130:
                 L = line.split()
@@ -13,31 +13,8 @@ def comparatora(tame0, tame1, tame2, tame3, wild0, wild1, wild2, wild3):
                 b = int(L[1], 16)
                 A.append(a)
                 Ak.append(b)
-    with open('tame-1.txt', 'r') as f:
-        for line in f:
-            if len(line) == 130:
-                L = line.split()
-                a = int(L[0], 16)
-                b = int(L[1], 16)
-                A.append(a)
-                Ak.append(b)
-    with open('tame-2.txt', 'r') as f:
-        for line in f:
-            if len(line) == 130:
-                L = line.split()
-                a = int(L[0], 16)
-                b = int(L[1], 16)
-                A.append(a)
-                Ak.append(b)
-    with open('tame-3.txt', 'r') as f:
-        for line in f:
-            if len(line) == 130:
-                L = line.split()
-                a = int(L[0], 16)
-                b = int(L[1], 16)
-                A.append(a)
-                Ak.append(b)
-    with open('wild-0.txt', 'r') as f:
+   
+    with open('check2.txt', 'r') as f:
         for line in f:
             if len(line) == 130:
                 L = line.split()
@@ -45,42 +22,17 @@ def comparatora(tame0, tame1, tame2, tame3, wild0, wild1, wild2, wild3):
                 b = int(L[1], 16)
                 B.append(a)
                 Bk.append(b)
-    with open('wild-1.txt', 'r') as f:
-        for line in f:
-            if len(line) == 130:
-                L = line.split()
-                a = int(L[0], 16)
-                b = int(L[1], 16)
-                B.append(a)
-                Bk.append(b)
-    with open('wild-2.txt', 'r') as f:
-        for line in f:
-            if len(line) == 130:
-                L = line.split()
-                a = int(L[0], 16)
-                b = int(L[1], 16)
-                B.append(a)
-                Bk.append(b)
-    with open('wild-3.txt', 'r') as f:
-        for line in f:
-            if len(line) == 130:
-                L = line.split()
-                a = int(L[0], 16)
-                b = int(L[1], 16)
-                B.append(a)
-                Bk.append(b)
+    
     result = list(set(A) & set(B))
     if len(result) > 0:
         sol_kt = A.index(result[0])
         sol_kw = B.index(result[0])
         d = Ak[sol_kt] - Bk[sol_kw]
         print('\n' + '\n' + 'SOLVED: ' + hex(d) + '\n')
-        print('  Tips: 1NULY7DhzuNvSDtPkFzNo6oRTZQWBqXNE9 ' + '\n')
-        file = open("BoooooooooooooooyahooooooooooooyaBs.txt", 'a')
+        file = open("KeyFound.txt", 'a')
         file.write("----------------------------------------\n")
         file.write(hex(Ak[sol_kt] - Bk[sol_kw]) + "\n")
         file.write("----------------------------------------\n")
-        file.write("Tips: 1NULY7DhzuNvSDtPkFzNo6oRTZQWBqXNE9\n")
         file.close()
         sys.exit(0)
     else:
@@ -88,16 +40,11 @@ def comparatora(tame0, tame1, tame2, tame3, wild0, wild1, wild2, wild3):
 
 def main():
 
-    s_tame0 = 'tame-0.txt'
-    s_tame1 = 'tame-1.txt'
-    s_tame2 = 'tame-2.txt'
-    s_tame3 = 'tame-3.txt'
-    s_wild0 = 'wild-0.txt'
-    s_wild1 = 'wild-1.txt'
-    s_wild2 = 'wild-2.txt'
-    s_wild3 = 'wild-3.txt'
-    #print('\n' + 'Compare:' + s_tame + s_wild + '\n')
-    comparatora(s_tame0, s_tame1, s_tame2, s_tame3, s_wild0, s_wild1, s_wild2, s_wild3)
+    s_tame0 = 'check1.txt'
+    s_wild0 = 'check2.txt'
+    
+    
+    comparatora(s_tame0, s_wild0)
     
 
 if __name__ == "__main__":
